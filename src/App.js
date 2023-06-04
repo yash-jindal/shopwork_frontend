52cd82cb71ea5fe4827dbb08ec72a93a660e133c
Yash Jindal <yjindal87@gmail.com>
2023-06-04 11:09:22 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import Updatecustomer from './updatecustomer/Updatecustomer';
 import Viewcustomer from './viewcustomer/Viewcustomer';
 import Entry from './entry/Entry';
 import UpdateEntry from './updateEntry/UpdateEntry';
+import { useEffect } from 'react';
+import axios from 'axios';
 
+const run = async () => {
+  try{
+    await axios.get("https://shopwork-back.onrender.com");
+  }catch(err){
+    throw err;
+  }
+    
+}
 function App() {
+  useEffect(() => {
+    run()
+  }, [])
   return (
     <div className="App">
       <BrowserRouter>
